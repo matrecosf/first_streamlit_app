2d628147d3947249a84188dd764338dd5f2e8cff
matrecosf <105363370+matrecosf@users.noreply.github.com>
2022-05-14 22:49:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,9 @@ streamlit.header('Fruityvice Fruit Advice')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized = pandas.json.normalize(fruityvice_response.json())
+
+fruityvice_normalized = pandas.json.normalize(fruityvice_response.json())
+
+streamlit.dataframe(fruityvice_normalized)
